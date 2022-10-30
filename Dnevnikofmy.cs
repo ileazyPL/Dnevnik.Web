7ffad9d68423825eda2d35b5f3dc1026196c99fe
Vladislav <Vladislav@DESKTOP-BIV25VK>
2022-10-30 18:13:07 +0300

    Добавьте файлы проекта.

---
+++ Dnevnikofmy.cs
@@ -0,0 +1,12 @@
+namespace MediumEditor.Web;
+
+public class Dnevnikofmy
+{
+    public string Date { get; set; }
+
+    public string Title { get; set; }
+
+    public string Text { get; set; }
+
+    public string? Summary { get; set; }
+}
---
+++ Models/NewPostModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MediumEditor.Web.Models
+{
+    public class NewPostModel
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public IFormFile File {get;set;}
+    }
+}
